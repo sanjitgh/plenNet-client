9edd0a210b42fd411c03fdc61e6855752e2f0aa3
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2025-01-04 20:07:37 +0600

    planet add features integrate

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,58 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { uploadImage } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const [uploadButtonText, setUploadButtonText] = useState({
+    name: "Upload Image",
+  });
+  const [loading, setLoading] = useState(false);
+  const axiosSecure = useAxiosSecure();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.target;
+    const name = form.name.value;
+    const category = form.category.value;
+    const description = form.description.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const imageURL = await uploadImage(image);
+
+    // saller info
+    const saller = {
+      name: user?.displayName,
+      email: user?.email,
+    };
+
+    // create plant data in a object
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imageURL,
+      saller,
+    };
+    // save plant in db
+    try {
+      await axiosSecure.post("/plants", plantData);
+      toast.success("Plant added successfully");
+      form.reset();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <Helmet>
@@ -9,9 +60,14 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uploadButtonText={uploadButtonText}
+        setUploadButtonText={setUploadButtonText}
+        loading={loading}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
